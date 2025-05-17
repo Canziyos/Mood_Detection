1b881649825cfba06eb621160e7eb45fe77bb8ce
Canziyos <canziyos1@gmail.com>
2025-05-17 15:17:18 +0200

    Reorganizing and experimenting in new dir

--- experiments/image_br/run_model.py
+++ experiments/image_br/run_model.py
@@ -5,9 +5,9 @@ sys.path.append(os.path.dirname(__file__))
 from mobilenetv2_emotion_recognizer import MobileNetV2EmotionRecognizer
 
 if __name__ == "__main__":
-    data_path = "../Full Dataset/Images"
+    data_path = "../Dataset/Images/train"
     model_path = "../models/mobilenetv2_emotion.pth"
-    test_image_path = "/Users/lejlapulic/Applied Artificial Intelligence Project/Mood_Detection/Full Dataset/Images/Angry/01-01-05-01-01-01-18_1.png" 
+    test_image_path = "test_ang.png" 
     recognizer = MobileNetV2EmotionRecognizer(data_dir=data_path, model_path=model_path)
 
     # Train the model

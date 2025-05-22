5cb345c6f602e988499477155247884d26236c74
ppn21001 <patrik.pettersson123@gmail.com>
2025-05-22 11:18:46 +0200

    Audiomodel now takes imgfile instead of soundfile

--- experiments/ex_audio/audio.py
+++ experiments/ex_audio/audio.py
@@ -15,60 +15,7 @@ import matplotlib.cm as cm
 
 class_names = ["Angry", "Disgust", "Fear", "Happy", "Neutral", "Sad"]
 num_classes = len(class_names)
-model_path = r"../models/mobilenetv2_aud.pth" # Path to the model- (I renamed the model i got from you)
-
-def audio_to_tensor(waveform, sample_rate):
-    
-    target_sample_rate = 16000
-    # -------------------- RESAMPLE AND EXTRACT LOG MEL SPECTROGRAM --------------------------------------------
-
-    # Resample if needed
-    if sample_rate != target_sample_rate:
-        resampler = torchaudio.transforms.Resample(orig_freq=sample_rate, new_freq=target_sample_rate)
-        waveform = resampler(waveform)
-
-    # Convert to mono
-    if waveform.shape[0] > 1:
-        waveform = waveform.mean(dim=0, keepdim=True)
-
-    # Compute log-Mel spectrogram (Big overlap for better time resolution, however, at expense of computation time)
-    mel_spectrogram = torchaudio.transforms.MelSpectrogram(
-        sample_rate=target_sample_rate,
-        n_fft=1024,
-        hop_length=64,
-        win_length=512,
-        window_fn=torch.hamming_window,
-        n_mels=128
-    )(waveform)
-
-    db_transform = torchaudio.transforms.AmplitudeToDB()
-    log_mel_spec = db_transform(mel_spectrogram)
-
-    # Normalize and apply "magma" colormap (To match training image orientation)
-    spec_np = log_mel_spec.squeeze().numpy()
-    spec_norm = (spec_np - spec_np.min()) / (spec_np.max() - spec_np.min() + 1e-9)
-    colormapped = cm.magma(spec_norm)[:, :, :3]  # Drop alpha channel
-    colormapped = (colormapped * 255).astype(np.uint8)
-
-    # Convert to PIL image
-    spec_img = Image.fromarray(colormapped)
-    spec_img = spec_img.resize((224, 224))
-    spec_img = spec_img.transpose(Image.FLIP_TOP_BOTTOM)
-
-    # Image transform
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                            std=[0.229, 0.224, 0.225])
-    ])
-
-    # Tensor
-    img_tensor = transform(spec_img).unsqueeze(0)
-    return img_tensor
-
-# --------------------------------------------------------------------------------------------------
-
+model_path = r"../models/mobilenetv2_aud_TrainedOnFullDataset_71.09.pth" # Path to the model- (I renamed the model i got from you)
 
 # -------------------- LOAD MODEL AND MAKE PREDICTION ----------------------------------------------
 
@@ -78,7 +25,7 @@ def audio_to_tensor(waveform, sample_rate):
 # This way, the model is loaded just once into memory (RAM or GPU), 
 # not from disk every time, and can be reused for all predictions.
 
-def load_audio_model(model_path="../models/mobilenetv2_aud.pth"):
+def load_audio_model(model_path="../models/mobilenetv2_aud_TrainedOnFullDataset_71.09.pth"):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = models.mobilenet_v2(weights=None)
     model.classifier[1] = torch.nn.Linear(model.last_channel, num_classes)
@@ -92,7 +39,20 @@ def load_audio_model(model_path="../models/mobilenetv2_aud.pth"):
 # and runs it through the loaded model via the second function. Then, as before, it produces
 #whatever it was producing.
 
-def audio_predict(model, img_tensor, device):
+def audio_predict(model, img_path, device):
+
+    # --------- IMAGE TRANSFORM ---------
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                            std=[0.229, 0.224, 0.225])
+    ])
+    # ----------------------------------
+
+    # Load and preprocess the image
+    img = Image.open(img_path).convert('RGB')
+    img_tensor = transform(img).unsqueeze(0).to(device)
     # Here is the completing step, we send the tensor
     # to the same place (device) as the model.
     img_tensor = img_tensor.to(device)
@@ -117,4 +77,3 @@ def audio_predict(model, img_tensor, device):
         softmax = torch.nn.Softmax(dim=1)
         outSoftmax = softmax(logits)
     return logits, outSoftmax, presoftMax, predicted_class
-

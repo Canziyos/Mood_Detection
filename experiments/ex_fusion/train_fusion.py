af781631a699f8d525efa08685ac200ee805bef4
Canziyos <canziyos1@gmail.com>
2025-05-23 11:07:36 +0200

    created thefinal fusion class

--- experiments/ex_fusion/train_fusion.py
+++ experiments/ex_fusion/train_fusion.py
@@ -6,7 +6,7 @@ ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
 if ROOT not in sys.path:
     sys.path.insert(0, ROOT)
 
-from src.fusion.AudioImageFusion import AudioImageFusion  # Make sure this is the correct (clean, logits-only) version!
+from src.fusion.AudioImageFusion import AudioImageFusion
 from dataloader import FlexibleFusionDataset, ConflictValDataset
 
 seed = 42
